259824e1d3d425e31ede064f9b4bade45ab88d46
maskeynihal <26411488+maskeynihal@users.noreply.github.com>
2023-04-30 21:35:46 +0545

    add authentication api in backend

--- server/config/sanctum.php
+++ server/config/sanctum.php
@@ -15,11 +15,22 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
-    ))),
+    'stateful' => explode(
+        ',',
+        env(
+            'SANCTUM_STATEFUL_DOMAINS',
+            sprintf(
+                '%s%s%s',
+                'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+                Sanctum::currentApplicationUrlWithPort(),
+                env('FRONTEND_URL')
+                  ? ','.
+                    parse_url(env('FRONTEND_URL'), PHP_URL_HOST).
+                    (parse_url(env('FRONTEND_URL'), PHP_URL_PORT) ? ':'.parse_url(env('FRONTEND_URL'), PHP_URL_PORT) : '')
+                  : ''
+            )
+        )
+    ),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,8 +52,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | This value controls the number of minutes until an issued token will be
-    | considered expired. If this value is null, personal access tokens do
-    | not expire. This won't tweak the lifetime of first-party sessions.
+    | considered expired. This will override any values set in the token's
+    | "expires_at" attribute, but first-party sessions are not affected.
     |
     */
 
